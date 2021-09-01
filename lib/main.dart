0059df7b25c13c81408e3464390c771b2fa74838
Prashant Ghimire <prashantghi8@gmail.com>
2021-09-01 08:08:03 +0545

    adds status icon, temperature and UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:liveweather/pages/home_page.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(brightness: Brightness.dark),
     home: HomePage(),
   ));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,101 +10,130 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future? currentWeather;
-//to get current weather as soon as app opens
+  //Future? currentWeather;
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: Colors.transparent,
+        //backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              FutureBuilder(
-                  future: getWeather(),
-                  builder: (context, AsyncSnapshot snapshot) {
-                    print(snapshot.data);
-                    if (snapshot.hasData) {
-                      var weatherData = snapshot.data;
-                      String? currentAddress =
-                          weatherData["location"]["name"].toString();
-                      String? currentCountry =
-                          weatherData["location"]["country"].toString();
-                      String? currentCelcius =
-                          weatherData["current"]["temp_c"].toString();
-                      String? currentFr =
-                          weatherData["current"]["temp_f"].toString();
-                      String? currentStatus = weatherData["current"]
-                              ["condition"]["text"]
-                          .toString();
-                      print("success");
-                      return Container(
-                        height: size.height,
-                        width: size.width,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                  "assets/cold_weather.png",
-                                ),
-                                fit: BoxFit.cover)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                color: Colors.white,
-                                child: Column(
-                                  children: [
-                                    Text(
-                                        "$currentCelcius Celcius, $currentFr F"),
-                                    Text(
-                                        "Address: $currentAddress/$currentCountry"),
-                                    Text("Weather Status: $currentStatus")
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                    if (!snapshot.hasData) {
-                      print("failure");
-                      return Center(
-                          child: Container(
-                        width: size.width,
-                        height: size.height,
-                        color: Colors.black,
+      child: Column(
+        children: [
+          FutureBuilder(
+              future: getWeather(),
+              builder: (context, AsyncSnapshot snapshot) {
+                print(snapshot.data);
+                if (snapshot.hasData) {
+                  var weatherData = snapshot.data;
+                  String? currentAddress =
+                      weatherData["location"]["name"].toString().toUpperCase();
+                  String? currentCountry = weatherData["location"]["country"]
+                      .toString()
+                      .toUpperCase();
+                  String? currentCelcius =
+                      weatherData["current"]["temp_c"].toString();
+                  String? currentStatus =
+                      weatherData["current"]["condition"]["text"].toString();
+                  String? statusIcon =
+                      weatherData["current"]["condition"]["icon"].toString();
+                  print(statusIcon);
+                  print("success");
+                  return Container(
+                    //color: Colors.white,
+                    height: size.height,
+                    width: size.width,
+                    // decoration: BoxDecoration(
+                    //     image: DecorationImage(
+                    //         image: AssetImage(
+                    //           "assets/bg.png",
+                    //         ),
+                    //         fit: BoxFit.cover)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SafeArea(
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Text(
-                              "No Internet, Refresh",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 25),
+                              "TODAY",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 30),
                             ),
-                            IconButton(
-                                onPressed: () {
-                                  print("pressed");
-                                  getWeather();
-                                  setState(() {});
-                                },
-                                icon: Icon(
-                                  Icons.refresh,
-                                  color: Colors.white,
-                                  size: 50,
-                                ))
+                            SizedBox(
+                              height: size.height * 0.01,
+                            ),
+                            Text(
+                              "$currentAddress, $currentCountry",
+                              style: TextStyle(
+                                  fontStyle: FontStyle.italic, fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: size.height * 0.08,
+                            ),
+                            Container(
+                              //color: Colors.blue,
+                              height: size.height * 0.3,
+                              width: size.height * 0.3,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage("http:$statusIcon"),
+                                      fit: BoxFit.cover)),
+                            ),
+                            Text(
+                              "$currentCelcius° C",
+                              style: TextStyle(
+                                  fontSize: 80, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: size.height * 0.03,
+                            ),
+                            Text(
+                              "$currentStatus",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
                           ],
                         ),
-                      ));
-                    }
+                      ),
+                    ),
+                  );
+                }
+                if (!snapshot.hasData) {
+                  print("failure");
+                  return Center(
+                      child: Container(
+                    width: size.width,
+                    height: size.height,
+                    color: Colors.black,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "No Internet, Refresh",
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                        IconButton(
+                            onPressed: () {
+                              print("pressed");
+                              getWeather();
+                              setState(() {});
+                            },
+                            icon: Icon(
+                              Icons.refresh,
+                              color: Colors.white,
+                              size: 50,
+                            ))
+                      ],
+                    ),
+                  ));
+                }
 
-                    return CircularProgressIndicator();
-                  }),
-            ],
-          ),
-        ));
+                return CircularProgressIndicator();
+              }),
+        ],
+      ),
+    ));
   }
 }
